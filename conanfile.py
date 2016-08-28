045a186a52b383fa59b675c37ce6ed1ef550e911
Javier G. Sogo <jgsogo@gmail.com>
2016-08-28 13:22:48 +0200

    working package

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+
+import os
+from conans import ConanFile
+from conans.tools import os_info, SystemPackageTool
+
+
+class PortaudioConan(ConanFile):
+    name = "portaudio"
+    version = "master"
+    settings = "os", "compiler", "build_type", "arch"
+    FOLDER_NAME = "portaudio_%s" % version.replace(".", "_")
+    url="https://github.com/jgsogo/conan-portaudio"
+    # exports = ["FindPortaudio.cmake"]
+    license=""  # TODO: Check licence
+
+    def system_requirements(self):
+        pack_name = None
+        if os_info.is_linux:
+            pack_name = "libasound-dev"
+
+        if pack_name:
+            installer = SystemPackageTool()
+            installer.update() # Update the package database
+            installer.install(pack_name) # Install the package
+
+    def source(self):
+        self.run("git clone https://git.assembla.com/portaudio.git {}".format(self.FOLDER_NAME))
+
+    def build(self):
+        command = './configure && make'
+        self.run("cd %s && %s" % (self.FOLDER_NAME, command))
+
+    def package(self):
+        self.copy("*.h", dst="include", src=os.path.join(self.FOLDER_NAME, "include"))
+        # self.copy("*.lib", dst="lib", src=os.path.join(self.FOLDER_NAME, "lib"))
+        self.copy("*.a", dst="lib", src=os.path.join(self.FOLDER_NAME, "lib", ".libs"))
+
+    def package_info(self):
+        self.cpp_info.libs = ["portaudio"]
