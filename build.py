ad22e55f4f25c03384e719a4b2dfbd825b52fed7
Javier G. Sogo <jgsogo@gmail.com>
2016-08-28 14:25:41 +0200

    just x64;2Q

--- build.py
+++ build.py
@@ -8,4 +8,9 @@ username = os.getenv("CONAN_USERNAME", "jgsogo")
 if __name__ == "__main__":
     builder = ConanMultiPackager(username=username)
     builder.add_common_builds()
+    filtered_builds = []
+    for settings, options in builder.builds:
+        if settings["compiler.arch"] == "x86_64":
+             filtered_builds.append([settings, options])
+    builder.builds = filtered_builds
     builder.run()
