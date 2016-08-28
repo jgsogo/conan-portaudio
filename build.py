25e4ded988c79a80cf76ad08873122a812f2f43d
Javier G. Sogo <jgsogo@gmail.com>
2016-08-28 14:32:04 +0200

    typo

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     builder.add_common_builds()
     filtered_builds = []
     for settings, options in builder.builds:
-        if settings["compiler.arch"] == "x86_64":
+        if settings["arch"] == "x86_64":
              filtered_builds.append([settings, options])
     builder.builds = filtered_builds
     builder.run()
