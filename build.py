dcb97461b41fe02c5c4f6842213027c32fa4d9f8
Javier G. Sogo <jgsogo@gmail.com>
2016-08-30 14:27:19 +0200

    override behaviour of Packager to upload only recipe

--- build.py
+++ build.py
@@ -5,8 +5,14 @@ import os
 username = os.getenv("CONAN_USERNAME", "jgsogo")
 
 
+class UploadRecipeOnly(ConanMultiPackager):
+    def run(self):
+        self._pack()
+        self._upload_packages(upload_all=False)
+
+
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username=username)
+    builder = UploadRecipeOnly(username=username)
     builder.add_common_builds(shared_option_name="Portaudio:shared")
     filtered_builds = []
     for settings, options in builder.builds:
