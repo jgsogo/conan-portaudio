4845bff19fc0b458e61db21414a6275c9f3f839b
Javier G. Sogo <jgsogo@gmail.com>
2017-05-03 11:03:04 +0200

    use only 'common builds'

--- build.py
+++ build.py
@@ -8,9 +8,4 @@ username = os.getenv("CONAN_USERNAME", "jgsogo")
 if __name__ == "__main__":
     builder = ConanMultiPackager(username=username)
     builder.add_common_builds(shared_option_name="Portaudio:shared")
-    filtered_builds = []
-    for settings, options in builder.builds:
-        if settings["arch"] == "x86_64":
-             filtered_builds.append([settings, options])
-    builder.builds = filtered_builds
     builder.run()
