7373b696077dbd184c456f638bbc60258f513922
Javier G. Sogo <jgsogo@gmail.com>
2017-05-03 12:14:24 +0200

    keep only release builds

--- build.py
+++ build.py
@@ -10,7 +10,10 @@ if __name__ == "__main__":
     builder.add_common_builds(shared_option_name="Portaudio:shared")
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        if settings["arch"] == "x86_64":
+        if settings["arch"] == "x86_64" and settings["build_type"] == "Release":
             filtered_builds.append([settings, options, env_vars, build_requires])
+    from pprint import pprint
+    pprint(filtered_builds)
+    exit()
     builder.builds = filtered_builds
     builder.run()
