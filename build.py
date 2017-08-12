8c0e37437dd920e53012fc622370f04d3b04d3c6
dimitri <dimi309@users.noreply.github.com>
2017-08-12 22:08:41 +0200

    build.py correction

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from conan.packager import ConanMultiPackager
 import os, platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username=username)
+    builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="Portaudio:shared")
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
